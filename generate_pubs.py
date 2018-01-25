0328c4964eeddc3dc0dfa1745c15a8974b38495b
Phillip Doehle <doehle@okstate.edu>
2018-01-25 14:21:16 -0600

    Remove technical report functionality
    There was only one technical report and it can be
    cited as an article. Therefore, I took this functionality
    out of the script.

--- generate_pubs.py
+++ generate_pubs.py
@@ -8,43 +8,61 @@ def main():
             formatted list of citations for website.')
     parser.add_argument('--articles', type = str, required=True, \
             help = "Name of .csv file with journal articles.")
+    '''
+    # Remove functionality for technical reports
+    # Treat technical reports as general articles
+    # Commented out thoughout code in case we change
+    # our mind.
+    
     parser.add_argument('--tech', type = str, required=True, \
             help = "Name of .csv file with technical reports.")
-    #
+    '''
+    '''
     # Don't put presentations on the website
     # Presentations have been commented out throughout the code
     # just in case we want to add this functionality back in later
-    #
-    #parser.add_argument('--presentations', type = str, required=True, \
-    #        help = "Name of .csv file with presentations.")
+    
+    parser.add_argument('--presentations', type = str, required=True, \
+            help = "Name of .csv file with presentations.")
+    '''
     parser.add_argument('--output', type = str, required=True, \
             help = "Name of ouput markdown file.")
 
     # Read in file names
     args = parser.parse_args()
     article_csv = args.articles
+    '''
     neup_csv = args.tech
-    #presentations_csv = args.presentations
+    presentations_csv = args.presentations
+    '''
     outfile_csv = args.output
 
     # Get publication lists
     articles = citation.getPubs(article_csv, "article")
+    '''
     neups = citation.getPubs(neup_csv, "neup_proj")
-    #presentations = citation.getPubs(presentations_csv, "presentation")
+    presentations = citation.getPubs(presentations_csv, "presentation")
+    '''
 
     # Make sure publication are in alphabetical order
     articles.sort(key = lambda x: x.authors)
+    '''
     neups.sort(key = lambda x: x.authors)
-    #presentations.sort(key = lambda x: x.authors)
+    presentations.sort(key = lambda x: x.authors)
+    '''
 
     # Get all years with publications
     years = []
     for pub in articles:
         years.append(pub.year)
+    '''
     for pub in neups:
         years.append(pub.year)
-    #for pub in presentations:
-    #    years.append(pub.year)
+    '''
+    '''
+    for pub in presentations:
+        years.append(pub.year)
+    '''
 
     # Remove non-unique values
     years = list(set(years))
@@ -56,19 +74,30 @@ def main():
         final_list.append("\n\n# " + year)
         # Articles
         if thatYearsPubs(year, articles):
+            '''
+            Now that the only type of publication
+            the scripts handles is articles,
+            we don't need the heading articles
+            under each year.
+            
             final_list.append("\n\n## Articles")
+            '''
             for pub in thatYearsPubs(year, articles):
                 final_list.append("\n + " + pub.cite())
+        '''
         # Technical Reports
         if thatYearsPubs(year, neups):
             final_list.append("\n\n## Technical Reports")
             for pub in thatYearsPubs(year, neups):
                 final_list.append("\n + " + pub.cite())
+        '''
+        '''
         # Presentations
-        #if thatYearsPubs(year, presentations):
-        #    final_list.append("\n\n## Presentations")
-        #    for pub in thatYearsPubs(year, presentations):
-        #        final_list.append("\n + " + pub.cite())
+        if thatYearsPubs(year, presentations):
+            final_list.append("\n\n## Presentations")
+            for pub in thatYearsPubs(year, presentations):
+                final_list.append("\n + " + pub.cite())
+        '''
 
     # Write output to markdown file
     final_list.append("\n")
