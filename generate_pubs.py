d7cffb6c3fedbad3e3cccf8c8d9c8e35001d15ec
Phillip Doehle <doehle@okstate.edu>
2018-09-07 16:40:19 -0500

    Bold year headings
    For some reason when I paste the final document from Word into
    the website content manager, it no longer bolds the year headings.
    I am now using the heading indicator and the emphasis indicator
    in the Markdown output.

--- generate_pubs.py
+++ generate_pubs.py
@@ -71,7 +71,7 @@ def main():
     # Final Formatted list
     final_list = []
     for year in years:
-        final_list.append("\n\n# " + year)
+        final_list.append("\n\n# **" + year + "**")
         # Articles
         if thatYearsPubs(year, articles):
             '''
